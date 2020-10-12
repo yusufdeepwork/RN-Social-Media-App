8d44345f9599683fc5bb6d4962899d01f73f9ac0
yusufdeepwork <yusuf.deepwork@gmail.com>
2020-10-12 21:49:46 +0300

    Stack Navigator feature is added to app.js

--- App.js
+++ App.js
@@ -2,13 +2,22 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {View, Text} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+const Stack = createStackNavigator();
+
+const HomeScreen = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View>
-        <Text>Hello React Native</Text>
-      </View>
-    </NavigationContainer>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      
+      </NavigationContainer>
   );
 }
