ae5363bfdd03365dc840f3803a31591acd0e8390
yusufdeepwork <yusuf.deepwork@gmail.com>
2020-10-12 22:06:53 +0300

    Detail Screen is added and Styles is edited

--- App.js
+++ App.js
@@ -1,23 +1,75 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 const Stack = createStackNavigator();
 
-const HomeScreen = () => {
+const HomeScreen = ({navigation}) => {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
+      <Button
+        title="go to details screen"
+        onPress={() => {
+          navigation.navigate('details');
+        }}
+      />
+    </View>
+  );
+};
+
+const DetailScreen = ({navigation}) => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Detail Screen</Text>
+      <Button
+        title="go to details screen...again"
+        onPress={() => {
+          navigation.push('Details');
+        }}
+      />
+      <Button
+        title="go to Home"
+        onPress={() => {
+          navigation.navigate('Home');
+        }}
+      />
+      <Button
+        title="Go Back"
+        onPress={() => {
+          navigation.goBack();
+        }}
+      />
+
+      <Button
+        title="Go To Firs Screen"
+        onPress={() => {
+          navigation.popToTop();
+        }}
+      />
     </View>
   );
 };
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Screen name="Home" component={HomeScreen} />
-      
-      </NavigationContainer>
+    <NavigationContainer
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '009387',
+        },
+        headerTintColo: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}>
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{title: 'Overview'}}
+      />
+      <Stack.Screen name="Details" component={DetailScreen} />
+    </NavigationContainer>
   );
 }
