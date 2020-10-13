12c904d4649a392e69ebf4901815c155947ab399
yusufdeepwork <yusuf.deepwork@gmail.com>
2020-10-13 16:45:44 +0300

    Drawer Content is added.

--- App.js
+++ App.js
@@ -3,13 +3,14 @@ import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import MainTabScreen from './screens/MainTabScreen';
+import {DrawerContent} from './screens/DrawerContent';
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Navigator drawerContent={(props) => <DrawerContent {...props} />}>
         <Drawer.Screen name="Home" component={MainTabScreen} />
         {/* <Drawer.Screen name="Details" component={DetailsStackScreen} /> */}
       </Drawer.Navigator>
---
+++ screens/DrawerContent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {DrawerContentScrollView, DrawerItem} from '@react-navigation/drawer';
+
+const DrawerContent = (props) => {
+  return (
+    <View>
+      <Text>This is a test..</Text>
+    </View>
+  );
+};
+
+export default DrawerContent;
+
+const styles = StyleSheet.create({});
