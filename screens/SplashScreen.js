99694d12ce96deca6a18cc9818eeb51d59501603
yusufdeepwork <yusuf.deepwork@gmail.com>
2020-10-18 12:52:11 +0300

    Spash Screen is updated

--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -1,21 +1,96 @@
 import React from 'react';
-import {StyleSheet, Text, View, Button} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Dimensions,
+  Image,
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import * as Animatable from 'react-native-animatable';
 
-const SplashScreen = () => {
+const SplashScreen = ({navigation}) => {
   return (
     <View style={styles.container}>
-      <Text>Splash Screen</Text>
-      <Button title="Click here" onPress={() => alert('Button Clicked')} />
+      <View style={styles.header}>
+        <Animatable.Image
+          animation="bounceIn"
+          duraton="1500"
+          source={require('../assets/logo.png')}
+          style={styles.logo}
+          resizeMode="stretch"
+        />
+      </View>
+      <Animatable.View animation="fadeInUpBig" style={styles.footer}>
+        <Text style={styles.title}>Stay connected with everyone! </Text>
+        <Text style={styles.text}>Sign in with Account </Text>
+        <View style={styles.button}>
+          <TouchableOpacity onPress={()=> navigation.navigate('SignInScreen')}>
+            <LinearGradient
+              colors={['#08d4c4', '#01ab9d']}
+              style={styles.SignIn}>
+              <Text style={styles.textSign}>Get Started</Text>
+              <MaterialIcons name="navigate-next" color="#fff" size={20} />
+            </LinearGradient>
+          </TouchableOpacity>
+        </View>
+      </Animatable.View>
     </View>
   );
 };
 
 export default SplashScreen;
 
+const {height} = Dimensions.get('screen');
+const height_logo = height * 0.28;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    backgroundColor: '#009387',
+  },
+  header: {
+    flex: 2,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  footer: {
+    flex: 1,
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 30,
+    borderBottomRightRadius: 30,
+    paddingVertical: 50,
+    paddingHorizontal: 30,
+  },
+  logo: {
+    width: height_logo,
+    height: height_logo,
+  },
+  title: {
+    color: '#05375a',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  text: {
+    color: 'grey',
+    marginTop: 30,
+  },
+  SignIn: {
+    width: 150,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 50,
+    flexDirection: 'row',
+  },
+  textSign: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  button: {
+    alignItems: 'flex-end',
+    marginTop: 30,
   },
 });
