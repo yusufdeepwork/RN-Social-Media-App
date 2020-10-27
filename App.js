04ceed4e846986f348a2aabe36bc226062d36ea2
yusufdeepwork <yusuf.deepwork@gmail.com>
2020-10-27 08:29:49 +0300

    Activitiy Indicator is added for Loading progress

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable require-jsdoc */
+/* eslint-disable max-len */
 import * as React from 'react';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
@@ -8,10 +11,30 @@ import BookmarkScreen from './screens/BookmarkScreen';
 import SettingsScreen from './screens/SettingsScreen';
 import SupportScreen from './screens/SupportScreen';
 import RootStackScreen from './screens/RootStackScreen';
+import {useEffect} from 'react';
+import {View} from 'react-native-animatable';
+import {ActivityIndicator} from 'react-native-paper';
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
+  // eslint-disable-next-line no-trailing-spaces
+  
+  // For Loading Progress
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(()=> {
+    setTimeout(()=> {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
+  if (isLoading) {
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}} >
+      <ActivityIndicator size="large" />
+    </View>;
+  }
+
   return (
     <NavigationContainer>
       <RootStackScreen />
