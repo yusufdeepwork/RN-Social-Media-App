00af659f40aeefd82a0a4d71ff411037b93fd578
yusufdeepwork <yusuf.deepwork@gmail.com>
2020-10-30 13:45:23 +0300

    example users data is added

---
+++ model/users.js
@@ -0,0 +1,20 @@
+export default Users = [
+  {
+    id: 1,
+    email: 'user1@gmail.com',
+    password: 'password',
+    userToken: 'token123',
+  },
+  {
+    id: 2,
+    email: 'user2@gmail.com',
+    password: 'password',
+    userToken: 'token123',
+  },
+  {
+    id: 3,
+    email: 'user3@gmail.com',
+    password: 'password',
+    userToken: 'token123',
+  },
+];
